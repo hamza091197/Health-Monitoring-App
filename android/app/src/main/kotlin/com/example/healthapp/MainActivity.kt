2950f8bafd1c3d6c3df1c8c3466fac38b40c4b62
Hamza <hk952078@gmail.com>
2024-11-14 12:22:07 +0500

    add new features

--- android/app/src/main/kotlin/com/example/healthapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/healthapp/MainActivity.kt
@@ -2,4 +2,7 @@ package com.example.healthapp
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    // No need to override anything for basic setup
+}
+
